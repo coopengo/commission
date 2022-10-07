7854da81525cd0beb1559a41dace5abf55e39944
Jean Cavallo <jean.cavallo@hotmail.fr>
2022-10-07 16:24:03 +0200

    [CUSTOM] Fix 1b77465 following merge

--- invoice.py
+++ invoice.py
@@ -119,12 +119,10 @@ class Invoice(metaclass=PoolMeta):
         pool = Pool()
         Commission = pool.get('commission')
 
-        invoices_to_revert_commission = [x for x in invoices if not x.move]
-
         super(Invoice, cls).cancel(invoices)
 
         to_delete = []
-        for sub_invoices in grouped_slice(invoices_to_revert_commission):
+        for sub_invoices in grouped_slice(invoices):
             ids = [i.id for i in sub_invoices]
             to_delete += cls._get_commissions_to_delete(ids)
             to_cancel = cls._get_commissions_to_cancel(ids)
